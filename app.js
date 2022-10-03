7ec17680174899ba5539c26bffd8d6b42cc7bbfb
Teddy Cubaka <teddycubaka96@gmail.com>
2022-10-03 15:56:19 +0100

    api optimization

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express")
 const mysql = require('mysql')
 const authorization = require('./db')
+const cors = require("cors")
 
 const app = express();
 app.use(express.json())
 
 const db = mysql.createConnection(authorization)
 
+app.use(cors());
+
+
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connecté à la base de données MySQL!");
 });
+
+app.get("/", (req, res) => {
+    res.json({
+        "type" : "success"
+    })
+})
 
 app.get('/api/contacts' , (req, res, next)=>{
     db.query("select * from contact",
